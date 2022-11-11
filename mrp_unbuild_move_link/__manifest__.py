7ac627423210ffa041034a57c03134163292e141
ThiagoMForgeFlow <thiago.mulero@forgeflow.com>
2022-11-11 09:29:29 +0100

    [14.0][ADD] mrp_unbuild_move_link: link the stock move of a ubuild with its MO

---
+++ mrp_unbuild_move_link/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Stock moves of manufacturing orders added to unbuild orders",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Manufacture",
+    "summary": "Link the stock moves of manufacturing orders to the respective unbuild orders",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture",
+    "depends": ["mrp_account"],
+    "data": ["views/stock_move_views.xml"],
+    "installable": True,
+}
